acf388514edddd3c5b7029728b6d7a47617205f8
mark <markkinuthia53@gmail.com>
2026-09-11 16:29:15 +0300

    refactor: update food list functionality and button label in HTML and JavaScript

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// find HTML elements and store them in JS
 const foodForm = document.getElementById("food-form");
 const foodNameInput = document.getElementById("food-name");
 const caloriesInput = document.getElementById("calories");
@@ -5,6 +6,26 @@ const foodList = document.getElementById("food-list");
 const totalCalories = document.getElementById("total-calories");
 const clearButton = document.getElementById("clear-button");
 
+// add event listener to the clear button
 clearButton.addEventListener("click", function () {
     totalCalories.textContent = "clear";
+});
+
+// add event listener to the form submission
+foodForm.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    // get the values from the input fields
+    const name = foodNameInput.value;
+    const calorieAmount = caloriesInput.value;
+
+    console.log(name);
+    console.log(calorieAmount);
+
+// create a new list item
+const foodItem = document.createElement("li");
+//give the list item text content
+foodItem.textContent = name + " - " + calorieAmount + " calories";
+// append(put) the list item to the food list
+foodList.appendChild(foodItem);
 });
